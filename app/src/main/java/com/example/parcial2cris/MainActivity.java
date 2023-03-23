645fc240c70f5f1d33b1b11927f7542f2051b485
cris251442 <126135249+cris251442@users.noreply.github.com>
2023-03-22 21:33:24 -0600

    Update1

--- app/src/main/java/com/example/parcial2cris/MainActivity.java
+++ app/src/main/java/com/example/parcial2cris/MainActivity.java
@@ -6,8 +6,24 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-ListView listaLeads;
+    private ArrayList<personas> listLeads;
+    ListView lv1;
+    Integer[] ImgPpersonas = {
+            R.drawable.lead_photo_1,
+            R.drawable.lead_photo_2,
+            R.drawable.lead_photo_3,
+            R.drawable.lead_photo_4,
+            R.drawable.lead_photo_5,
+            R.drawable.lead_photo_6,
+            R.drawable.lead_photo_7,
+            R.drawable.lead_photo_8,
+            R.drawable.lead_photo_9,
+            R.drawable.lead_photo_10
+    };
+ArrayList<personas> listaLeads;
 
 
     @Override
@@ -15,7 +31,21 @@ ListView listaLeads;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listaLeads = (ListView) findViewById(R.id.lvLead);
+        listaLeads=new ArrayList<personas>();
+
+        listaLeads.add(new personas("Alexander Pierrot"));
+        listaLeads.add(new personas("Carlos Lopez"));
+        listaLeads.add(new personas("Sara Bonz"));
+        listaLeads.add(new personas("Liliana Clarence"));
+        listaLeads.add(new personas("Benito Peralta"));
+        listaLeads.add(new personas("Juan Jaramillo"));
+        listaLeads.add(new personas("Christian Steps"));
+        listaLeads.add(new personas("Alexa Giraldo"));
+        listaLeads.add(new personas("Linda Murillo"));
+        listaLeads.add(new personas("Lizeth Astrada"));
+
+
+        listaLeads = (ListView) findViewById(R.id.lvLeads);
 
         ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this,R.array.arrayLeads, android.R.layout.simple_list_item_1);
 
---
+++ app/src/main/java/com/example/parcial2cris/personas.java
@@ -0,0 +1,4 @@
+package com.example.parcial2cris;
+
+public class personas {
+}
