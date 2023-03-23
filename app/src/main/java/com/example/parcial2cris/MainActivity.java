257f7261429640aff3fa401f8fdf708646ba9e8d
cris251442 <126135249+cris251442@users.noreply.github.com>
2023-03-22 20:48:45 -0600

    New updates

--- app/src/main/java/com/example/parcial2cris/MainActivity.java
+++ app/src/main/java/com/example/parcial2cris/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.parcial2cris;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+ListView listaLeads;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaLeads = (ListView) findViewById(R.id.lvLead);
+
+        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this,R.array.arrayLeads, android.R.layout.simple_list_item_1);
+
+        listaLeads.setAdapter(adaptador);
     }
 }
